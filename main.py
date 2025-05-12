6b922a2bc6391fce2521c96727845c613a2831f5
penguin-wings <gytisnaujasis@icloud.com>
2025-05-13 01:21:43 +0300

    upd: color, behavior changes
    change design, add saving with keyboard inputs without buttons, document

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def start_calculating(event):
 
     elif event.char:
         user_text += event.char
-        timer = window.after(5000, reset_app)
+        timer = window.after(1000, reset_app)
 
     return
 
@@ -27,16 +27,18 @@ def start_calculating(event):
 def reset_app():
     global timer, user_text
     typing_area.delete('1.0', 'end')
-    user_text = ""
+    typing_area.insert("1.0", "$ ")
+    user_text += "\n"
     timer = None
     return
 
 
-def save_text():
+def save_text_to_file(e=None):
     global user_text
     if user_text == "":
         return
     try:
+        # TODO generating infinite files if one exists or show it exists and options to choose 
         f = open('writeups.txt', 'r')
     except FileNotFoundError:
         f = open('writeups.txt', 'w')
@@ -47,12 +49,13 @@ def save_text():
         cont = f.read()
         if cont == "":
             text_to_write = user_text
+        # TODO -> check if text is the same? Can use an identifier when built as OOP
         else:
             text_to_write = f'\n{user_text}'
 
         with open('writeups.txt', 'a') as f:
             f.write(text_to_write)
-            user_text = ""
+            # user_text += "\n"
     finally:
         return
 
@@ -62,8 +65,8 @@ def save_text():
 # -----------------------------------------------------------------------------------------
 
 BORDER = "#3C2C3E"
-FG = 'khaki'
-BG = "#4B5D67"
+FG = 'white'
+BG = "#000000"
 
 FONT_FAMILY1 = 'Calibri'
 FONT_FAMILY2 = 'Helvetica'
@@ -85,28 +88,34 @@ instruction = "If you don't press any key for 5 seconds, the text you have writt
 
 window = Tk()
 window.title('Disappearing Text Desktop App')
-window.config(bg=BG, padx=20, pady=10)
+window.config(bg=BG, padx=0, pady=0)
 
-heading = Label(text=heading, font=HEAD_FONT, bg=BG, fg=FG, padx=10, pady=10)
-instruction = Label(text=instruction, font=PARA_FONT2,
-                    fg=FG, bg=BG, pady=10)
-typing_area = Text(font=PARA_FONT, bg=BG, fg=FG, width=100, height=15, wrap='w',
-                   highlightcolor=BORDER, highlightthickness=4, highlightbackground=BORDER,
-                   padx=5, pady=5)
+heading = Label(text=heading, font=HEAD_FONT, bg=BG, fg=FG, padx=0, pady=0)
+instruction = Label(text=instruction, font=PARA_FONT2, fg=FG, bg=BG, pady=0)
+
+typing_area = Text(font=PARA_FONT,  bg=BG, fg=FG, width=100, height=15, wrap='w',
+                   highlightcolor=BORDER, highlightthickness=0, highlightbackground=BORDER,
+                   padx=0, pady=0, insertbackground="#fff", insertborderwidth=0, insertwidth=10)
+
+typing_area.insert("1.0", "$ ")
+typing_area.event_generate("<<Savingfile>>")
+typing_area.event_add('<<Savingfile>>', '<Control-s>')
+typing_area.bind('<<Savingfile>>', save_text_to_file)
 typing_area.bind('<KeyPress>', start_calculating)
+
 reset_btn = Button(text='Reset', fg=FG, bg=BG, font=PARA_FONT,
                    highlightbackground=FG, highlightcolor=FG, highlightthickness=0, border=3,
                    command=reset_app, width=50)
 
 save_btn = Button(text='Save', fg=FG, bg=BG, font=PARA_FONT,
                    highlightbackground=FG, highlightcolor=FG, highlightthickness=0, border=3,
-                   command=save_text, width=50)
+                   command=save_text_to_file, width=50)
 
-heading.grid(row=0, column=0, columnspan=3)
-instruction.grid(row=2, column=0, columnspan=3)
+# heading.grid(row=0, column=0, columnspan=3)
+# instruction.grid(row=2, column=0, columnspan=3)
 typing_area.grid(row=3, column=0, columnspan=3)
-reset_btn.grid(row=4, column=0)
-save_btn.grid(row=4, column=2)
+# reset_btn.grid(row=4, column=0)
+# save_btn.grid(row=4, column=2)
 
 
 window.mainloop()
